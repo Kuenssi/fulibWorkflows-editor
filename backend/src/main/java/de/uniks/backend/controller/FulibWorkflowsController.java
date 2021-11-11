9bfd157409c74081920161b32ec8fc579750659a
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-11-11 11:26:59 +0100

    feat(BE): enable cors

--- backend/src/main/java/de/uniks/backend/controller/FulibWorkflowsController.java
+++ backend/src/main/java/de/uniks/backend/controller/FulibWorkflowsController.java
@@ -1,10 +1,12 @@
 package de.uniks.backend.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin()
 public class FulibWorkflowsController {
 
     @RequestMapping("/generateWorkflowBoard")
