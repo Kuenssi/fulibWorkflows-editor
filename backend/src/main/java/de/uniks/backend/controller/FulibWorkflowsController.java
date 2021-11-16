be6a7a523055e9026f6c71940583c856900b8821
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-11-16 14:22:57 +0100

    feat(BE): add temprary workaround for workflow generation

--- backend/src/main/java/de/uniks/backend/controller/FulibWorkflowsController.java
+++ backend/src/main/java/de/uniks/backend/controller/FulibWorkflowsController.java
@@ -1,5 +1,6 @@
 package de.uniks.backend.controller;
 
+import org.fulib.workflows.html.HtmlGenerator3;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,13 +8,36 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @Controller
 @CrossOrigin()
 public class FulibWorkflowsController {
 
-    @PostMapping(path = "/generate", consumes = MediaType.ALL_VALUE)
+    @PostMapping(path = "/generate", consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
     String generate(@RequestBody String yamlData) {
-        return yamlData;
+        String result = "";
+
+        HtmlGenerator3 htmlGenerator3 = new HtmlGenerator3();
+        try {
+            File tempFile = File.createTempFile("test", ".yaml");
+
+            Files.writeString(Path.of(tempFile.getPath()), yamlData);
+
+            htmlGenerator3.generateViewFiles(tempFile.getPath(), "Testerino"); // TODO tmp file for now -> Write new method for direct data input in fulibWorkflows later
+
+            tempFile.deleteOnExit();
+
+            result = Files.readString(Path.of("./tmp/Testerino/TesterinoEventStorming.html"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 }
