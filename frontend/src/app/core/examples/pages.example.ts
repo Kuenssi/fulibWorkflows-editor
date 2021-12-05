20d5e0052f8cb72651b108a4542490eddd342583
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-12-05 17:27:32 +0100

    feat(FE): fix examples

--- frontend/src/app/core/examples/pages.example.ts
+++ frontend/src/app/core/examples/pages.example.ts
@@ -2,22 +2,22 @@ export const pagesExample =
   '- workflow: Pages\n' +
   '\n' +
   '- page:\n' +
-  '\t- name: Register\n' +
+  '    - name: Register\n' +
   '    - text: Please register yourself\n' +
   '    - input: E-Mail\n' +
   '    - input: Username\n' +
   '    - password: Password\n' +
   '    - password: Repeat Password\n' +
   '    - button: Register\n' +
-  ' \n' +
+  '\n' +
   '- page:\n' +
-  '\t- name: Login\n' +
+  '    - name: Login\n' +
   '    - text: Welcome back\n' +
   '    - input: Username/E-Mail\n' +
   '    - password: Password\n' +
   '    - button: Login\n' +
-  '    \n' +
+  '\n' +
   '- page:\n' +
-  '\t- name: Overview\n' +
+  '    - name: Overview\n' +
   '    - text: Your current Purchases\n' +
   '    - button: Add Purchase';
