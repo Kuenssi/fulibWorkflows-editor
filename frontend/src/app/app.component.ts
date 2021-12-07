69bec07a7d1951f161d1b5ab1022e3bc6286ca7b
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2021-12-07 13:19:32 +0100

    feat(FE): finish up gutter with angular split

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, NgZone, OnInit, ViewChild} from '@angular/core';
 
 import {pmExample} from './core/examples/pm.example';
 import {msExample} from './core/examples/ms.example';
+import {IOutputData, SplitComponent} from 'angular-split';
 import {pagesExample} from './core/examples/pages.example';
 import {GenerateResult} from './core/model/GenerateResult';
 import {createMapFromAnswer} from './core/helper/map.helper';
@@ -15,6 +16,7 @@ import {MockupViewerComponent} from './components/mockup-viewer/mockup-viewer.co
 })
 export class AppComponent implements OnInit {
   @ViewChild('mockupViewer') private mockupViewerComponent!: MockupViewerComponent;
+  @ViewChild("split") split!: SplitComponent
 
   //Codemirror
   public content!: any;
@@ -24,6 +26,8 @@ export class AppComponent implements OnInit {
 
   public currentExampleDesc: string = 'Select example';
   public examplesList = ['Data Modelling', 'Microservices', 'Pages'];
+
+  public showIframeHider = false
 
   constructor(private fulibWorkflowsService: FulibWorkflowsService,
               private zone: NgZone) {
@@ -86,5 +90,21 @@ export class AppComponent implements OnInit {
   setPageFromIframe(index: number) {
     console.log(index);
     this.mockupViewerComponent.setCurrentPageIndex(index + 1); // +1 because index is zero based
+  }
+
+  // Source: https://github.com/angular-split/angular-split/blob/main/src/app/examples/iframes/iframes.component.ts
+  dragStartHandler() {
+    this.showIframeHider = true
+  }
+
+  dragEndHandler() {
+    this.showIframeHider = false
+  }
+
+  splitGutterClick({ gutterNum }: IOutputData) {
+    // By default, clicking the gutter without changing position does not trigger the 'dragEnd' event
+    // This can be fixed by manually notifying the component
+    // See issue: https://github.com/angular-split/angular-split/issues/186
+    this.split.notify('end', gutterNum)
   }
 }
