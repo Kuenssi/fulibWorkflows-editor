a10f8667deb92836d9c293ec628542687477de6c
Maximilian Freiherr von Künßberg <maximilian-kuenssberg@t-online.de>
2022-02-24 15:27:23 +0100

    fix frontend hint from fill to value

--- frontend/src/codemirror/addon/hint/fulibWorkflows-hint.ts
+++ frontend/src/codemirror/addon/hint/fulibWorkflows-hint.ts
@@ -1,7 +1,7 @@
 import * as CodeMirror from 'codemirror';
 
 const noteKeywords = ['workflow', 'externalSystem', 'service', 'command', 'event', 'policy', 'user', 'data', 'page', 'problem'];
-const pageKeywords = ['pageName', 'text', 'input', 'password', 'button', 'fill', 'targetPage']
+const pageKeywords = ['pageName', 'text', 'input', 'password', 'button', 'value', 'targetPage']
 
 // @ts-ignore
 CodeMirror.registerHelper('hint', 'fulibWorkflows', (cm) => {
